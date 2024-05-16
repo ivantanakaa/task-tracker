0aacce8f8eeabc60ecaf35648b81a2f55ec75e9a
Ivan Tanaka <ivantanaka@Ivans-MacBook-Air.local>
2024-05-16 14:37:06 +0700

    feat: changing favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Task Tracker",
+  title: "Tasks Trackery",
   description: "Track your task here.",
 };
 
@@ -21,6 +21,11 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         />
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
